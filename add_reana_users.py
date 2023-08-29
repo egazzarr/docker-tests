26d3dd403e47c04eb5adcfd09623b911edc79071
root <root@egazzarr-develop.cern.ch>
2023-08-29 10:33:08 +0200

    access token

--- add_reana_users.py
+++ add_reana_users.py
@@ -12,14 +12,16 @@ import subprocess
 client_id = os.environ['CLIENT_ID']
 client_secret  = os.environ['CLIENT_SECRET']  
 reana_admin_token = os.environ['REANA_ADMIN_TOKEN']
+print(reana_admin_token)
 
 with open('/home/emails.json', 'r') as file:
     j = json.load(file)
-    print(j)
+    # print(j)
 for i in j:
-    print (i)
+    # print (i)
     # output = subprocess.check_output([f'kubectl exec -i -t deployment/reana-server -n reana -- flask reana-admin user-create --email {i} --admin-access-token ${REANA_ADMIN_TOKEN}'], shell=True, encoding='utf-8')
-    output = subprocess.check_output([f'flask reana-admin user-create --email {i} --admin-access-token ${REANA_ADMIN_TOKEN}'], shell=True, encoding='utf-8')
+    output = subprocess.check_output([f'flask reana-admin user-create --email {i} --admin-access-token {reana_admin_token}'], shell=True, encoding='utf-8')
+    # output = subprocess.check_output([f'echo {i} {reana_admin_token}'], shell=True, encoding='utf-8')
     
     print(output)
    
